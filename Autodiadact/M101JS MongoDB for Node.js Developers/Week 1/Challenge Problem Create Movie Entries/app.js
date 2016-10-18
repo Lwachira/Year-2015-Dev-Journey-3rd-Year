f3162e080e5272697d8857754fdcff5a4d09af21
Lincoln <blazeboy.wachira@gmail.com>
2016-10-18 17:30:17 +0200

    Working posts

--- Autodiadact/M101JS MongoDB for Node.js Developers/Week 1/Challenge Problem Create Movie Entries/app.js
+++ Autodiadact/M101JS MongoDB for Node.js Developers/Week 1/Challenge Problem Create Movie Entries/app.js
@@ -3,26 +3,27 @@ var express = require('express'),
     engines = require('consolidate'),
     bodyParser = require('body-parser'),
     MongoClient = require('mongodb').MongoClient,
-    asserts = require('assert');
+    assert = require('assert');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Handler for internal server errors
 function errorHandler(err, req, res, next) {
     console.error(err.message);
     console.error(err.stack);
     res.status(500).render('error_template', { error: err });
-
 }
 
 MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
-    asserts.equal(null, err);
+
+    assert.equal(null, err);
     console.log("Successfully connected to MongoDB.");
 
     app.get('/', function (req, res, next) {
-        res.render('/add_movie', {});
+        res.render('add_movie', {});
     });
 
     app.post('/add_movie', function (req, res, next) {
@@ -33,11 +34,10 @@ MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
         if ((title == '') || (year == '') || (imdb == '')) {
             next('Please provide an entry for all fields.');
         } else {
-            db.collection('movies').insertOne({
-                'title': title, 'year': year, 'imdb': imdb
-            },
+            db.collection('movies').insertOne(
+                { 'title': title, 'year': year, 'imdb': imdb },
                 function (err, r) {
-                    assert.equal(null, r);
+                    assert.equal(null, err);
                     res.send("Document inserted with _id: " + r.insertedId);
                 }
             );
